4d9271b5cb3d289b521afd523c4ab254b4cddfec
Samet Çiftçi <temasictfic@gmail.com>
2026-02-10 23:43:24 +0300

    Bump version to 0.6.0 and add tikv-jemallocator for Linux memory allocation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[cfg(target_os = "linux")]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 use std::env;
 use std::path::PathBuf;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
